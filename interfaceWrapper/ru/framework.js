3c570818c3147448a1bbb37e7c928d8d6bc5302d
Alina <alina.listunova@gmail.com>
2016-05-18 18:39:20 +0300

    Task [3]

--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -9,12 +9,22 @@ function newSandboxFor(appName){
     module: {},
     console: console,
     // Помещаем ссылку на fs API в песочницу
-    fs: fs
+    // Task #3 Передаем в песочницу склонированный интерфейс
+    fs: cloneInterface(fs)
     // Task #2 Удаляем обертку таймера
   };
   // Преобразовываем хеш в контекст
   context.global = context;
   return vm.createContext(context);
+}
+
+// Task #3 Функция для клонирования интерфейса
+function cloneInterface(anInterface) {
+  var clone = {};
+  for (var key in anInterface) {
+    clone[key] = anInterface[key];
+  }
+  return clone;
 }
 
 // Читаем исходный код приложения из файла
