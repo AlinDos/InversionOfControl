ffe9cc3e969eab2683c68c829ed287d72aeb063a
Alina <alina.listunova@gmail.com>
2016-05-18 03:35:16 +0300

    Task [2]

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,10 +1,9 @@
 // Вывод из глобального контекста модуля
 console.log('From application global context');
 
-// Объявляем функцию для события таймера
-function timerEvent() {
-  console.log('From application timer event');
-}
-
-// Устанавливаем функцию на таймер
-setTimeout(timerEvent, 1000);
+// Task #2 Удаляем вызов таймера и добавляем работу с файлом
+var fileName = './README.md';
+console.log('Application going to read ' + fileName);
+fs.readFile(fileName, function(err, src) {
+  console.log('File ' + fileName + ' size ' + src.length);
+});
