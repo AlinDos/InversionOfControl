eb4df4415337924ae0a9ee4be3598c1ce8904225
Alina <alina.listunova@gmail.com>
2016-05-18 10:10:46 +0300

    Make function of creating sandbox

--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -3,22 +3,27 @@
 var fs = require('fs'),
     vm = require('vm');
 
-// Объявляем хеш из которого сделаем контекст-песочницу
-var context = {
-  module: {},
-  console: console,
-  // Помещаем ссылку на fs API в песочницу
-  fs: fs
-  // Task #2 Удаляем обертку таймера
-};
-
-// Преобразовываем хеш в контекст
-context.global = context;
-var sandbox = vm.createContext(context);
+// Функция создания контекста-песочницы
+function newSandboxFor(appName){
+  var context = {
+    module: {},
+    console: console,
+    // Помещаем ссылку на fs API в песочницу
+    fs: fs
+    // Task #2 Удаляем обертку таймера
+  };
+  // Преобразовываем хеш в контекст
+  context.global = context;
+  return vm.createContext(context);
+}
 
 // Читаем исходный код приложения из файла
 var fileName = './application.js';
 fs.readFile(fileName, function(err, src) {
+  
+  // Создаем новую песочницу
+  var sandbox = newSandboxFor(fileName);
+  
   // Запускаем код приложения в песочнице
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
